f780a00c8f98dddc99033b509e3ac77e6c091416
Nikolai Romantsov <nsromantsov@edu.hse.ru>
2023-04-08 22:39:17 +0300

    style fix

--- generate_settings.py
+++ generate_settings.py
@@ -3,8 +3,8 @@ import os
 
 if __name__ == "__main__":
     if len(args := sys.argv) != 3:
-        print("You should provide 2 args. Token and G-Cloud project name\nExample:\n"
-              "python generate_settings.py my_token my_project")
+        print("\nYou should provide 2 args. Token and G-Cloud project name\n\nExample:\n"
+              "python generate_settings.py my_token my_project\n")
     else:
         try:
             if os.path.exists("settings.ini"):
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                 out.write(f"[BOT]\n"
                           f"bot_token = {args[1]}\n\n"
                           f"[GCLOUD]\n"
-                          f"project_name = {args[2]}")
+                          f"project_name = {args[2]}\n")
             print("settings.ini created!")
         except Exception:
             print("cant create settings.ini")
